1faaa5ec1b2e423a54553dabf0236e27c1593456
Al Gus <llityka1@yandex.ru>
2025-04-05 14:49:00 +0300

    feat: добавил компонент движения врагу

---
+++ Assets/Root/DmitryEnvironment/Scripts/Agents/Enemy/EnemyAgent.cs
@@ -0,0 +1,37 @@
+namespace Root.Rak.Agents.Enemy
+{
+    public class EnemyAgent
+    {
+
+    }
+
+    public class EnemyBrain
+    {
+
+    }
+
+    public class EnemyAvatar
+    {
+
+    }
+
+    public class EnemyModel
+    {
+
+    }
+
+    public class EnemyEyes
+    {
+
+    }
+
+    public class EnemyAttacker
+    {
+        
+    }
+
+    public class EnemyAnimator
+    {
+
+    }
+}
