6015abe1942c98f3f87e481ef2926dca0a5d1824
Maxim S. <its3zin.main@gmail.com>
2025-04-05 20:50:36 +0300

    feat(project): edit bullt

---
+++ Assets/Root/MaximEnvironement/Scripts/Character/CharacterBullet.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using InfimaGames.LowPolyShooterPack.Legacy;
+using Root.Rak.Agents;
+using Root.Rak.Tests;
+using UnityEngine;
+
+namespace Root.MaximEnvironment
+{
+    public class CharacterBullet : MonoBehaviour
+    {
+        public Collider Collider;
+
+        private void Awake()
+        {
+            Collider = GetComponent<Collider>();
+            
+            Collider.isTrigger = true;
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            if (other.gameObject.TryGetComponent<IEntityAttacked>(out var attacked))
+                if (attacked.ID == TeamID.AGENT)
+                    attacked.TakeDamage(new TestAttack(10f));
+        }
+    }
+}
