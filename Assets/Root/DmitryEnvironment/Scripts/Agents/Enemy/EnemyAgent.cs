8a0d6d7b08f27bf44189c9fca91662dc5cceb524
Al Gus <llityka1@yandex.ru>
2025-04-05 15:59:20 +0300

    feat: добавил первичный компонент для сборки врага

--- Assets/Root/DmitryEnvironment/Scripts/Agents/Enemy/EnemyAgent.cs
+++ Assets/Root/DmitryEnvironment/Scripts/Agents/Enemy/EnemyAgent.cs
@@ -1,26 +1,39 @@
+using UnityEngine;
+using UnityEngine.AI;
+
 namespace Root.Rak.Agents.Enemy
 {
-    public class EnemyAgent
+    [RequireComponent(typeof(Animator), typeof(NavMeshAgent))]
+    public class EnemyAgent : MonoBehaviour
     {
+        private EnemyAnimator _animator;
 
+        private EnemyMotion _motion;
+
+        private EnemyModel _model;
+
+        private EnemyBrain _brain;
+
+        public void Start()
+        {
+            _animator = new EnemyAnimator(GetComponent<Animator>(), GetComponentInChildren<ChildrenAnimatorHandler>());
+
+            _motion = new EnemyMotion(GetComponent<NavMeshAgent>(), null);
+
+            _model = new EnemyModel();
+
+            _brain = new EnemyBrain(_model, _animator, _motion);
+        }
+
+        public void Update()
+        {
+            _motion.Update();
+
+            _brain.Update();
+        }
     }
 
-    public class EnemyBrain
-    {
-
-    }
-
-    public class EnemyAvatar
-    {
-
-    }
-
-    public class EnemyModel
-    {
-
-    }
-
-    public class EnemyEyes
+/*    public class EnemyAvatar
     {
 
     }
@@ -28,5 +41,5 @@ namespace Root.Rak.Agents.Enemy
     public class EnemyAttacker
     {
         
-    }
+    }*/
 }
