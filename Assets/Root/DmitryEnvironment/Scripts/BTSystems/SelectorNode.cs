3d67d4f4a2398222e98a0bd83dbfebcfc511f180
Al Gus <llityka1@yandex.ru>
2025-04-05 14:37:15 +0300

    feat: сделал систему BT

---
+++ Assets/Root/DmitryEnvironment/Scripts/BTSystems/SelectorNode.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace Root.Rak.BT
+{
+    public class SelectorNode : ACollectionNode
+    {
+        public SelectorNode()
+            => _children = new List<ABTNode>();
+
+        public SelectorNode(List<ABTNode> nodes)
+            => _children = nodes;
+
+        public override NodeStatus Tick()
+        {
+            foreach (var node in _children)
+            {
+                if (node.Tick() == NodeStatus.SUCCESS)
+                    return NodeStatus.SUCCESS;
+
+                else if (node.Tick() == NodeStatus.RUNNING)
+                    return NodeStatus.RUNNING;
+            }
+
+            return NodeStatus.FAILURE;
+        }
+    }
+}
