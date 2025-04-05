21653a2d861921f35e9aba83767c5055ea6c78f3
Al Gus <llityka1@yandex.ru>
2025-04-05 15:53:20 +0300

    feat: добавил лог-модель врагу

---
+++ Assets/Root/DmitryEnvironment/Scripts/Agents/Enemy/EnemyModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Root.Rak.Agents.Enemy
+{
+    public class EnemyModel
+    {
+        public bool IsLife { get; private set; }
+        public bool IsDead { get; private set; }
+        public bool HasTarget { get; private set; }
+
+        public void UpdateTarget()
+        {
+            HasTarget = true;
+        }
+    }
+}
