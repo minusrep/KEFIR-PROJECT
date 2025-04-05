326b69827a647887819d4a44f6227a5be5a0bc02
Al Gus <llityka1@yandex.ru>
2025-04-05 21:55:20 +0300

    feat: начал делать визитора

---
+++ Assets/Root/DmitryEnvironment/Scripts/Agents/Visitor/VisitorAgent.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Root.Rak.Agents.Visitor
+{
+    public class VisitorAgent : MonoBehaviour
+    {
+        
+    }
+
+    public class VisitorModel
+    {
+
+    }
+
+    public class VisitorMotion
+    {
+
+    }
+}
---
+++ Assets/Root/DmitryEnvironment/Scripts/Agents/Visitor/VisitorBrain.cs
@@ -0,0 +1,19 @@
+﻿using Root.Rak.BT;
+using System.Collections.Generic;
+using UnityEngine.UIElements;
+
+namespace Root.Rak.Agents.Visitor
+{
+    public class VisitorBrain
+    {
+        private SelectorNode _root;
+
+        public void Build()
+        {
+            _root = new SelectorNode(new List<ABTNode>
+            {
+
+            });
+        }
+    }
+}
