8b3abef2e976e2d36249fcb640270468ba9821f2
Al Gus <llityka1@yandex.ru>
2025-04-05 18:22:59 +0300

    feat: добавил id команды для детекта объекта, которого атакуют

--- Assets/Root/DmitryEnvironment/Scripts/Agents/IEntityAttacked.cs
+++ Assets/Root/DmitryEnvironment/Scripts/Agents/IEntityAttacked.cs
@@ -1,7 +1,11 @@
 ﻿namespace Root.Rak.Agents
 {
+    public enum TeamID { PLAYER, AGENT }
+
     public interface IEntityAttacked
     {
+        TeamID ID { get; }
+
         void TakeDamage(IAttack attack);
     }
 }
